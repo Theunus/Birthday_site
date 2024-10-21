34b56f73ad12f76444c5773d2838502aba87bb5b
Theunus O'kelly <157046071+Theunus@users.noreply.github.com>
2024-10-21 18:03:29 +0200

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,18 @@
-from flask import Flask
-from flask_bootstrap5 import Bootstrap  
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
-import os
+from flask import Flask, render_template
 
-# Get the current directory of the script
-current_dir = os.path.dirname(os.path.abspath(__file__))
-template_dir = os.path.join(current_dir, 'Templates')
+app = Flask(__name__)
 
-app = Flask(__name__, template_folder=template_dir)  # Ensure it reflects the Docker container structure
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-# Updated DATABASE_URL for 'pro-track' database
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', '12345')
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "mysql+pymysql://tokelly:theunusok33@localhost/pro_track?charset=utf8mb4")
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+@app.route('/funny')
+def funny():
+    return render_template('funny.html')
 
-bootstrap = Bootstrap(app)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
-
-from app import routes
+if __name__ == '__main__':
+    app.run(debug=True)
