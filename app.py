b103c606b5fbdf4056ad839a2191ba740d2aaf88
Theunus O'kelly <157046071+Theunus@users.noreply.github.com>
2024-10-24 16:14:43 +0200

    added more messages

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import os
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
@@ -16,6 +14,11 @@ def about():
 @app.route('/funny')
 def funny():
     return render_template('funny.html')
+
+# Redirect from root URL to home page if accessed incorrectly
+@app.route('/<path:path>')
+def catch_all(path):
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
